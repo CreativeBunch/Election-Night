e54e26827acef692524f5c5e2138eae16fbf27d5
Jessica Dembe <jmdembe@gmail.com>
2016-11-04 14:44:41 -0400

    Created ajax call in data and deleted extraneous lines in ui

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -2,4 +2,16 @@
     'use strict';
     window.electionNight = window.electionNight || {};
 
+    window.electionNight.newCandidate = function newCandidate (name, avatar_url,intelligence, charisma, willpower) {
+        $.ajax({
+            url: '/candidates',
+            dataType: 'json',
+            method: 'POST'
+            headers: {
+
+            }
+            
+        });
+    }
+
 })();
