626634ff352a6cf7e43a62f89008ce67112699a5
Jessica Dembe <jmdembe@gmail.com>
2016-11-05 11:41:38 -0400

    Added GET ajax call to data js file

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -11,15 +11,16 @@
               'Content-Type': 'application/json'
             }//end of headers for POST ajax call
           .done(function handleCandidate(data) {
+              event.preventDefault()
               console.log("it worked!");
               console.log(data);
           })//end done click hanlder
-          .fail(function handleCandidateError(xhr) {
-              if (xhr > 400 || xhr < 499) {
+          .fail(function handleCandidateError(xhr, errorType) {
+              if (errorType > 400 || errorType < 499) {
                   console.log('Oh no! Client error. please check your data and re-submit.');
               }
 
-              if (xhr > 500 || xhr <599) {
+              if (errorType > 500 || errorType <599) {
                   console.log('Oh no! Server error! We need to make sure our server is working as it should');
               }
           })//ends fail event handler
@@ -31,9 +32,22 @@
             url: '/candidates',
             dataType: 'json'
             headers: {
-
-            }
+                'Content-Type': 'application/json'
+            }//end of header for getCandidates
         })
-    }
+        .done(function handleGetCandidatesSuccess(data) {
+            console.log("Call successful");
+        })//end of ajax call for getCandidates
+        .fail(function handleGetCandidatesError(xhr, errorType) {
+            console.log("ERROR");
+            if (errorType > 400 || errorType < 499) {
+                console.log('Oh no! Client error. please check your data and re-submit.');
+            }
+
+            if (errorType > 500 || errorType <599) {
+                console.log('Oh no! Server error! We need to make sure our server is working as it should');
+            }//end of 
+        })
+    }//end of function for getCandidates
 
 })();
