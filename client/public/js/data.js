6e8c422101b8635e1e0b3961a9918fb3e1b8ddec
Jessica Dembe <jmdembe@gmail.com>
2016-11-05 11:05:18 -0400

    committing and pushing this change again

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -9,11 +9,11 @@
             method: 'POST'
             headers: {
               'Content-Type': 'application/json'
-            }
+            }//end of headers for POST ajax call
           .done(function handleCandidate(data) {
               console.log("it worked!");
               console.log(data);
-          })
+          })//end done click hanlder
           .fail(function handleCandidateError(xhr) {
               if (xhr > 400 || xhr < 499) {
                   console.log('Oh no! Client error. please check your data and re-submit.');
@@ -22,8 +22,10 @@
               if (xhr > 500 || xhr <599) {
                   console.log('Oh no! Server error! We need to make sure our server is working as it should');
               }
-          })
-        });
-    }
+          })//ends fail event handler
+        });//end of ajax call for GET /candidates
+    }//ends new candidate function
+
+    
 
 })();
